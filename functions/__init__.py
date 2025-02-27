5addefadd55b319e131c506151ae8358badc7554
Nico <a.nicolah@gmail.com>
2025-02-27 10:03:24 -0300

    hago un breve ejemplo de como usar el flash de fede

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,4 +1,5 @@
 from functions.kr_functions import log_points, ab_fraction
+from functions.flash_fede import flash_gamma_gamma
 
 
-__all__ = ["log_points", "ab_fraction"]
+__all__ = ["log_points", "ab_fraction", "flash_gamma_gamma"]
